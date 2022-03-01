9d134c8b266d26d3dce239978829303a8ffab8d3
Didenko17 <vladislav.didenko.2001@gmail.com>
2022-03-01 18:02:29 +0300

    Project init

---
+++ m9lta-ui/src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+
+</style>
